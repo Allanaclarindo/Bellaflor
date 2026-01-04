e230884baa0df5e12de292fdf881f4e051b05826
Allanaclarindo <lanaclarindo2@gmail.com>
2026-01-03 22:31:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -23,15 +23,30 @@ fetch('produtos.json')
 function renderizarProdutos() {
   lista.innerHTML = '';
   produtos.forEach((p, index) => {
-    lista.innerHTML += `
-      <div class="produto">
-        <img src="${p.imagem}" alt="${p.nome}" onclick="verImagem('${p.imagem}')">
-        <h3>${p.nome}</h3>
-        <p>${p.preco}</p>
-        <p><strong>Cores:</strong> ${p.cores}</p>
-        <p><strong>Tamanhos:</strong> ${p.tamanhos}</p>
-        <button onclick="abrirModal(${index})">Comprar</button>
-      </div>
+  let imagensHTML = '';
+
+if (p.imagens && p.imagens.length > 1) {
+  imagensHTML = `
+    <img src="${p.imagens[0]}" alt="${p.nome}" onclick="verImagem('${p.imagens[0]}')">
+    <img src="${p.imagens[1]}" alt="${p.nome}" onclick="verImagem('${p.imagens[1]}')">
+  `;
+} else {
+  const img = p.imagem || p.imagens?.[0];
+  imagensHTML = `
+    <img src="${img}" alt="${p.nome}" onclick="verImagem('${img}')">
+  `;
+}
+
+lista.innerHTML += `
+  <div class="produto">
+    ${imagensHTML}
+    <h3>${p.nome}</h3>
+    <p>${p.preco}</p>
+    <p><strong>Cores:</strong> ${p.cores}</p>
+    <p><strong>Tamanhos:</strong> ${p.tamanhos}</p>
+    <button onclick="abrirModal(${index})">Comprar</button>
+  </div>
+`;
     `;
   });
 }
